e5aee724db2957f1bc9c66d5488b334c561c2ddf
pante <panterua@hotmail.com>
2020-08-16 20:19:52 -0300

    init page

---
+++ docs/init.js
@@ -0,0 +1,41 @@
+import * as firebase from 'firebase/app';
+import '@firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLR4W3kuEHnPgASLvEOLLYp-VkQ46t12U",
+  authDomain: "subscriddit.firebaseapp.com",
+  databaseURL: "https://subscriddit.firebaseio.com",
+  projectId: "subscriddit",
+  storageBucket: "subscriddit.appspot.com",
+  messagingSenderId: "969737676662",
+  appId: "1:969737676662:web:eee8ffb919e6f4fe44fc57"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js", { scope: "/" })
+    .then(function (reg) {
+      if (reg.installing) {
+        console.log("Service worker installing");
+      } else if (reg.waiting) {
+        console.log("Service worker installed");
+      } else if (reg.active) {
+        console.log("Service worker active");
+      }
+
+      messaging.getToken().then(function (token) {
+        console.log("Token:", token);
+      })
+      .catch(function (err) {
+        console.log("Error:", err);
+      });
+      
+    })
+    .catch(function (error) {
+      console.log("Registration failed with:", error);
+    });
+}
---
+++ docs/sw.js
@@ -0,0 +1,37 @@
+import * as firebase from 'firebase/app';
+import '@firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLR4W3kuEHnPgASLvEOLLYp-VkQ46t12U",
+  authDomain: "subscriddit.firebaseapp.com",
+  databaseURL: "https://subscriddit.firebaseio.com",
+  projectId: "subscriddit",
+  storageBucket: "subscriddit.appspot.com",
+  messagingSenderId: "969737676662",
+  appId: "1:969737676662:web:eee8ffb919e6f4fe44fc57"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.getToken().then(function(token){ 
+  console.log("TOKEN:", token);
+})
+.catch(function(err){ 
+  console.log("ERROR:", err);
+});
+
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
