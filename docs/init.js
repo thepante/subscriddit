89e20c9c92370e66b572c85283f3cdb38aaacd30
pante <panterua@hotmail.com>
2020-08-21 18:29:16 -0300

    up

--- docs/init.js
+++ docs/init.js
@@ -1,20 +1,9 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCLR4W3kuEHnPgASLvEOLLYp-VkQ46t12U",
-  authDomain: "subscriddit.firebaseapp.com",
-  databaseURL: "https://subscriddit.firebaseio.com",
-  projectId: "subscriddit",
-  storageBucket: "subscriddit.appspot.com",
-  messagingSenderId: "969737676662",
-  appId: "1:969737676662:web:eee8ffb919e6f4fe44fc57"
-};
-
-firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
 const requestSW = () => {
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./firebase-messaging-sw.js", { scope: "/subscriddit/" })
+    navigator.serviceWorker.register("https://thepante.github.io/subscriddit/firebase-messaging-sw.js", { scope: "/subscriddit/" })
       .then(function (reg) {
         if (reg.installing) {
           console.log("Service worker installing");
