f819128083bcd098cb9d3eb628045eb384dcaa5b
pante <panterua@hotmail.com>
2020-08-21 17:57:19 -0300

    up

--- docs/init.js
+++ docs/init.js
@@ -1,6 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCLR4W3kuEHnPgASLvEOLLYp-VkQ46t12U",
+  authDomain: "subscriddit.firebaseapp.com",
+  databaseURL: "https://subscriddit.firebaseio.com",
+  projectId: "subscriddit",
+  storageBucket: "subscriddit.appspot.com",
+  messagingSenderId: "969737676662",
+  appId: "1:969737676662:web:eee8ffb919e6f4fe44fc57"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
-
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("./sw.js", { scope: "/subscriddit/" })
