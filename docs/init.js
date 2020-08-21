eb136da9684dec90c65b2d582a40f83cef1b6987
pante <panterua@hotmail.com>
2020-08-21 18:04:32 -0300

    up

--- docs/init.js
+++ docs/init.js
@@ -12,26 +12,34 @@ firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("./sw.js", { scope: "/subscriddit/" })
-    .then(function (reg) {
-      if (reg.installing) {
-        console.log("Service worker installing");
-      } else if (reg.waiting) {
-        console.log("Service worker installed");
-      } else if (reg.active) {
-        console.log("Service worker active");
-      }
+const requestSW = () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js", { scope: "/subscriddit/" })
+      .then(function (reg) {
+        if (reg.installing) {
+          console.log("Service worker installing");
+        } else if (reg.waiting) {
+          console.log("Service worker installed");
+        } else if (reg.active) {
+          console.log("Service worker active");
+        }
 
-      messaging.getToken().then(function (token) {
-        console.log("Token:", token);
+        messaging.getToken().then(function (token) {
+          console.log("Token:", token);
+        })
+        .catch(function (err) {
+          console.log("Error:", err);
+        });
+        
       })
-      .catch(function (err) {
-        console.log("Error:", err);
+      .catch(function (error) {
+        console.log("Registration failed with:", error);
       });
-      
-    })
-    .catch(function (error) {
-      console.log("Registration failed with:", error);
-    });
+  }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("notificationsRequest").addEventListener("click", () => {
+    requestSW();
+  });
+});
